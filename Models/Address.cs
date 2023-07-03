6df1d5548c2a4b37f1f0212af3333396d83d4f17
guwanc <guwanc@gmail.com>
2023-07-03 11:24:30 +0500

    Add Address model file

---
+++ Models/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace welcome_api.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string AddressText { get; set; }
+        public int StudentId { get; set; }
+    }
+}
